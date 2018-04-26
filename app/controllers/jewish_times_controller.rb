da1143a52750d2fb15ae2b0f0ff131e7ca493b6b
Barry <bdc1130@yahoo.com>
2018-04-25 21:11:22 -0500

    adding index.html and index.js

--- app/controllers/jewish_times_controller.rb
+++ app/controllers/jewish_times_controller.rb
@@ -3,16 +3,13 @@ class JewishTimesController < ApplicationController
 
     def index
        # user enters zipcode
-       # zipcode = 60645
        input_zip = params[:zip]
-       # input_date = 041518
 
 
        # zipcode is converted into longitude and latitude
        lat_long = Geocoder.coordinates(input_zip)
 
         day = Time.now
-        # day = 3.months.ago
 
         latitude = lat_long[0]
         longitude = lat_long[1]
@@ -33,21 +30,7 @@ class JewishTimesController < ApplicationController
         number = [60.minutes, 52.minutes, 50.minutes, 45.minutes, 35.minutes]
         index = 0
         input_array = []
-        tallis = 0
         
-
-        
-        # earliest time for tallis and tefillin 1 hour before sunrise Rabbi Henkin
-        # rising - 60.minutes
-        # 52 minutes before sunrise Olat Tamid
-        # rising - 52.minutes
-        # 50 minutes before sunrise Olat Tamid
-        # rising - 50.minutes
-        # 45 minutes before sunrise Olat Tamid
-        # rising - 45.minutes
-        # 35 - 40 minutes before sunrise Reb Moshe
-        # rising - 35.minutes
-
 
     5.times do
 
@@ -59,23 +42,10 @@ class JewishTimesController < ApplicationController
     end
         
 
-
-        render json: input_array[0]
-
-        # render json: rising - 60.minutes
-
-        # render json: sun_times.set(day, latitude, longitude).in_time_zone(night)
+        # render json: setting
+        render json: input_array
 
 
-    
-
-       #  the equation of sunrise minus 60 minutes 
-       # output is sunrise and sunset with earliest times for tallis and tefillin
-       # p response
-       
-
-
-# renders json: {message: "your times are "}
     end
 
 end
